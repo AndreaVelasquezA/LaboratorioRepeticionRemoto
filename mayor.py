9d7c591bde7ba95eb0c1b4e34790cfdd014cca37
Andrea <andrea.velasquez.2019@upb.edu.co>
2019-08-24 06:30:00 +0200

    ejercicio resuelto

--- mayor.py
+++ mayor.py
@@ -1,7 +1,21 @@
-a=int(input("digite el numero:"))
-if a>0:
-    print("el numero es mayor que cero")
-else:
-    print("no es mayor que cero")
+cant=10
+positivos=0
+negativos=0
+ceros=0
+for i in range(1, cant+1):
+ num=int(input("digite el numero:"))
+ if num>0:
+      positivos=(positivos+1)
+ else:
+       if num<0:
+          negativos=(negativos+1)
+       else num==0:
+          ceros=(ceros+1)
+      fin si
+  fin si
+fin para
+print("positivos:",positivos)
+print("negativos",negativos)
+print("ceros",ceros)
 
 
