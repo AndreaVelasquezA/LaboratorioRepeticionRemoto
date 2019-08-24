e4b347a80079b285023e10be0f4f7218e54122b2
Andrea <andrea.velasquez.2019@upb.edu.co>
2019-08-24 11:58:14 +0200

    ejercicio dos resuelto

--- pares.py
+++ pares.py
@@ -1,5 +1,15 @@
-a=int(input("digite el numero:"))
-if a%2==0:
-    print("es positivo")
-else:
-    print("no es positivo")
+cant=int(input("digite la cantidad de numeros:"))
+pares=0
+impares=0
+for i in range(1, cant+1):
+     num=int(input("digite el numero:"))
+     if num==0:
+        break
+     else:
+        if num%2==0:
+           pares=(pares+1)
+        else:
+            impares=(impares+1)
+
+print("pares:",pares)
+print("impares:",impares)
